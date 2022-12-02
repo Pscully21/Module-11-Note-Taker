af60335898d8d7e5a7b82e747ff42749cc2cde85
Parker Scully <pscully05@gmail.com>
2022-12-01 23:21:43 -0500

    Added most code waiting for Heroku functionality

---
+++ Develop/routes/apiRoutes/notesRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { createNote, deleteNote } = require('../../lib/notes');
+let { notesArray } = require('../../db/notes');
+
+router.get('/notes', (req, res) => {
+    let results = notesArray;
+    res.json(results);
+});
+
+router.post('/notes', (req, res) => {
+    if(notesArray){
+        req.body.id = notesArray.length.toString();
+    } else
+    {req.body.id = 0}
+    res.json(createNote(req.body, notesArray));
+});
+router.delete('/notes/:id', async (req, res) => {
+    const { id } = req.params
+    notesArray = await deleteNote(id, notesArray);
+    res.json(notesArray);
+});
+module.exports = router;
